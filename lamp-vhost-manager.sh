efaa177c7693c5adf757f734ea7fad2327d2de00
Marko Martinović <marko@techytalk.info>
2015-01-02 22:53:01 +0100

    Update lamp-vhost-manager.sh
    Refactor Apache 2.4 branch, add support for specifying HTTP port, add support for creating HTTPS virtual host and specifying HTTPS port

--- lamp-vhost-manager.sh
+++ lamp-vhost-manager.sh
@@ -32,11 +32,17 @@ MYSQLP=
 # Desired MySQL database name (enter to avoid having to use this argument)
 MYSQLN=
 
+# HTTP port
+PORT=80
+
+# HTTPS port
+PORTSSL=443
+
 # Initialize git repository
 GIT=false
 
-# Create CGI directory in document root
-CGI=false
+# Create SSL vhost
+SSL=false
 
 ###############################################################################
 
@@ -70,21 +76,24 @@ function usage() {
     -m    Mode (required, "add" or "remove")
     -n    Project name (required, used as directory name and as domain name if -t is omitted)
     -t    TLD (optional, provide only if directory name differs from domain name)
-    -d    Document root (optional, "$DOCROOT" by default)
+    -d    Document root (optional, defaults to "$DOCROOT")
+    -o    HTTP port (optional, defaults to port 80)
+    -S    Create HTTPS virtual host (optional, defaults to no, requires ssl-cert package installed)
+    -s    HTTPS port (optional, defaults to port 443, to be used with -S option)
     -u    MySQL administrative user name (optional, ommit to avoid managing database)
     -p    MySQL administrative user password (optional, ommit to avoid managing database)
-    -U    Desired MySQL database user name (optional, to be used with -u and -p, project name by default)
-    -P    Desired MySQL database password (optional, to be used with -u and -p, project name by default)
-    -N    Desired MySQL database name (optional, to be used with -u and -p, project name by default)
-    -g    Initialize empty git repository inside project directory
-    -c    Create CGI directory in document root
+    -U    Desired MySQL database user name (optional, to be used with -u and -p, project name by default, trimmed to 16 characters)
+    -P    Desired MySQL database password (optional, to be used with -u and -p, project name by default, trimmed to 16 characters)
+    -N    Desired MySQL database name (optional, to be used with -u and -p, project name by default, trimmed to 16 characters)
+    -g    Initialize empty git repository inside project directory (optional, defaults to no)
+
 
   Examples:
     -Add project "example.loc" and create database having "example.loc" user and password and name:
-	$0 -m add -n example.loc -u mysqladminusername -p mysqladminuserpassword
+	$0 -m add -n example.loc -u root -p mysqladminuserpassword
 
     -Remove project "example.loc" and optionaly remove database having "example.loc" user and password and name:
-	$0 -m remove -n example.loc -u mysqladminusername -p mysqladminuserpassword
+	$0 -m remove -n example.loc -u root -p mysqladminuserpassword
 
     -Add project "example.loc" using "example" as directory name and "example.loc" as domain without creating database:
 	$0 -m add -n example -t loc
@@ -93,10 +102,10 @@ function usage() {
 	$0 -m remove -n example -t loc
 
     -Add project "example.loc" and create database having "exampledbname" name, "exampledbuser" user and "exampledbpass" password:
-	$0 -m add -n example.loc -u mysqladminusername -p mysqladminuserpassword -U exampledbuser -P exampledbpass -N exampledbname
+	$0 -m add -n example.loc -u root -p mysqladminuserpassword -U exampledbuser -P exampledbpass -N exampledbname
 
     -Remove project "example.loc" and optionaly remove database having "exampledbname" name, "exampledbuser" user and "exampledbpass" password:
-	$0 -m remove -n example.loc -u mysqladminusername -p mysqladminuserpassword -U exampledbuser -P exampledbpass -N exampledbname
+	$0 -m remove -n example.loc -u root -p mysqladminuserpassword -U exampledbuser -P exampledbpass -N exampledbname
 EOF
 }
 
@@ -105,46 +114,26 @@ function add() {
     # Create virtualhost document root
     if [ ! -d $VHOSTDOCROOT ]
     then
-	echo "Creating \"$VHOSTDOCROOT\"..."
-	mkdir $VHOSTDOCROOT
+	    echo "Creating \"$VHOSTDOCROOT\"..."
+	    mkdir $VHOSTDOCROOT
 
-     # Create git repository
-      if [ $GIT == true ]
-      then
-        echo "Creating git repository in \"$VHOSTDOCROOT\"..."
-        git init $VHOSTDOCROOT
-      fi
-
+         # Create git repository
+        if [ $GIT == true ]
+        then
+            echo "Creating git repository in \"$VHOSTDOCROOT\"..."
+            git init $VHOSTDOCROOT
+        fi
     else
-	echo "\"$VHOSTDOCROOT\" already exists, so not creating..."
+    	echo "\"$VHOSTDOCROOT\" already exists, so not creating..."
     fi
 
     # Create virtualhost log root
     if [ ! -d $VHOSTLOGROOT ]
     then
-	echo "Creating \"$VHOSTLOGROOT\"..."
-	mkdir $VHOSTLOGROOT
+	    echo "Creating \"$VHOSTLOGROOT\"..."
+	    mkdir $VHOSTLOGROOT
     else
-	echo "\"$VHOSTLOGROOT\" already exists, so not creating..."
-    fi
-
-    if [ $CGI == true ]
-    then
-	echo "Creating \"$VHOSTCGIROOT\"..."
-	mkdir $VHOSTCGIROOT
-read -r -d '' CGIPATTERN <<EOF
-    ScriptAlias /cgi-bin/ $VHOSTCGIROOT/
-    <Directory "$VHOSTCGIROOT">
-	AllowOverride None
-	Options +ExecCGI -MultiViews +SymLinksIfOwnerMatch
-	#Order allow,deny
-	#Allow from all
-	Require all granted
-    </Directory>
-EOF
-    else
-	echo "\"$VHOSTCGIROOT\" already exists, so not creating..."
-	CGIPATTERN=
+	    echo "\"$VHOSTLOGROOT\" already exists, so not creating..."
     fi
 
     # Detect user and group ownerships (for serving outside of /var/www)
@@ -157,71 +146,109 @@ EOF
     if [ "$DOCROOTUSER" != "$VHOSTDOCROOTUSER" ]
     then
     	echo "Chown \"$VHOSTDOCROOT\" to \"$DOCROOTUSER\"..."
-	chown -R $DOCROOTUSER $VHOSTDOCROOT
+	    chown -R $DOCROOTUSER $VHOSTDOCROOT
     else
-	echo "\"$VHOSTDOCROOT\" already owned by user \"$DOCROOTUSER\", so not changing ownership..."
+	    echo "\"$VHOSTDOCROOT\" already owned by user \"$DOCROOTUSER\", so not changing ownership..."
     fi
 
     # Chgrp virtualhost document root to group owning document root if neccessary
     if [ "$DOCROOTGROUP" != "$VHOSTDOCROOTGROUP" ]
     then
     	echo "Chgrp \"$VHOSTDOCROOT\" to \"$DOCROOTGROUP\"..."
-	chgrp -R $DOCROOTGROUP $VHOSTDOCROOT
+	    chgrp -R $DOCROOTGROUP $VHOSTDOCROOT
     else
-	echo "\"$VHOSTDOCROOT\" already owned by user \"$DOCROOTUSER\" from group \"$DOCROOTGROUP\", so not changing group ownership..."
+	    echo "\"$VHOSTDOCROOT\" already owned by user \"$DOCROOTUSER\" from group \"$DOCROOTGROUP\", so not changing group ownership..."
     fi
 
     # Add line to "/etc/hosts" if it isn't already there
     grep -Fxq "$HOSTSLINE" "/etc/hosts"
     if [ $? = 1 ]
     then
-	echo "Adding \"$HOSTSLINE\" to \"/etc/hosts\"..."
-	echo "$HOSTSLINE" >> /etc/hosts
+	    echo "Adding \"$HOSTSLINE\" to \"/etc/hosts\"..."
+	    echo "$HOSTSLINE" >> /etc/hosts
     else
-	echo "\"$HOSTSLINE\" already inside \"/etc/hosts\", so not adding..."
+	    echo "\"$HOSTSLINE\" already inside \"/etc/hosts\", so not adding..."
     fi
 
     # Create virtual host file if it doesn't already exist
     if [ ! -f $VHOSTFILE ]
     then
-	echo "Creating \"$VHOSTFILE\"..."
-cat > $VHOSTFILE <<EOF
-<VirtualHost *:80>
+	    echo "Creating \"$VHOSTFILE\"..."
+        cat > $VHOSTFILE <<EOF
+<VirtualHost *:$PORT>
     ServerAdmin webmaster@$VHOSTDOMAIN
     ServerName $VHOSTDOMAIN
 
     DocumentRoot $VHOSTDOCROOT
     <Directory />
-	Options FollowSymLinks
-	AllowOverride None
+        Options FollowSymLinks
+        AllowOverride None
     </Directory>
     <Directory $VHOSTDOCROOT/>
-	Options Indexes FollowSymLinks MultiViews
-	AllowOverride All
-	#Order allow,deny
-	#allow from all
-	Require all granted
+        Options Indexes FollowSymLinks MultiViews
+        AllowOverride All
+        #Order allow,deny
+        #allow from all
+        Require all granted
     </Directory>
-    $CGIPATTERN
+
     CustomLog $VHOSTLOGROOT/access.log combined
     ErrorLog $VHOSTLOGROOT/error.log
 </VirtualHost>
 EOF
     else
-	echo "\"$VHOSTFILE\" already exists, so not creating..."
+	    echo "\"$VHOSTFILE\" already exists, so not creating..."
+    fi
+
+    # Create SSL virtual host file if required ...
+    if [ $SSL == true ]
+    then
+        # ... and if it doesn't already exist
+        if [ ! -f $VHOSTFILESSL ]
+        then
+	        echo "Creating \"$VHOSTFILESSL\"..."
+cat > $VHOSTFILESSL <<EOF
+<VirtualHost *:$PORTSSL>
+    ServerAdmin webmaster@$VHOSTDOMAIN
+    ServerName $VHOSTDOMAIN
+
+    SSLEngine On
+    SSLCertificateFile	/etc/ssl/certs/ssl-cert-snakeoil.pem
+    SSLCertificateKeyFile /etc/ssl/private/ssl-cert-snakeoil.key
+
+    DocumentRoot $VHOSTDOCROOT
+    <Directory />
+        Options FollowSymLinks
+        AllowOverride None
+    </Directory>
+    <Directory $VHOSTDOCROOT/>
+        Options Indexes FollowSymLinks MultiViews
+        AllowOverride All
+        #Order allow,deny
+        #allow from all
+        Require all granted
+    </Directory>
+
+    CustomLog $VHOSTLOGROOT/access.log combined
+    ErrorLog $VHOSTLOGROOT/error.log
+</VirtualHost>
+EOF
+        else
+	    echo "\"$VHOSTFILESSL\" already exists, so not creating..."
+        fi
     fi
 
     # If MySQL credentials are available, use them to create db and user
     if [[ ! -z $MYSQLAU ]] || [[ ! -z $MYSQLAP ]]
     then
-    echo "Creating MySQL \"$MYSQLU\" user and \"$MYSQLN\" database..."
-mysql "-u$MYSQLAU" "-p$MYSQLAP" <<QUERY_INPUT
+        echo "Creating MySQL \"$MYSQLU\" user and \"$MYSQLN\" database..."
+MYSQL_PWD=$MYSQLAP mysql "-u$MYSQLAU" <<QUERY_INPUT
 GRANT USAGE ON * . * TO '$MYSQLU'@'localhost' IDENTIFIED BY '$MYSQLP' WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;
 CREATE DATABASE IF NOT EXISTS \`$MYSQLN\`;
 GRANT ALL PRIVILEGES ON \`$MYSQLN\`. * TO '$MYSQLU'@'localhost';
 QUERY_INPUT
     else
-	echo "Ommit creating MySQL user and database..."
+	    echo "Omit creating MySQL user and database..."
     fi
 
     # Enable virtual host
@@ -238,9 +265,9 @@ QUERY_INPUT
 
     if [[ ! -z $MYSQLAU ]] || [[ ! -z $MYSQLAP ]]
     then
-	echo "MYSQL USER: $MYSQLU"
-	echo "MYSQL PASSWORD: $MYSQLP"
-	echo "MYSQL DATABASE: $MYSQLN"
+	    echo "MYSQL USER: $MYSQLU"
+	    echo "MYSQL PASSWORD: $MYSQLP"
+	    echo "MYSQL DATABASE: $MYSQLN"
     fi
 }
 
@@ -248,64 +275,73 @@ function remove() {
     # Remove virtualhost document root if it exists
     if [ -d $VHOSTDOCROOT ]
     then
-	# Ask for confirmation
-	yes_no_pause "Do you want to remove \"$VHOSTDOCROOT\"?"
-	if [ $? = 0 ]
-	then
-	    echo "Removing \"$VHOSTDOCROOT\"..."
-	    rm -fR $VHOSTDOCROOT
-	else
-	    echo "NOT removing \"$VHOSTDOCROOT\"..."
-	fi
+	    # Ask for confirmation
+	    yes_no_pause "Do you want to remove \"$VHOSTDOCROOT\"?"
+	    if [ $? = 0 ]
+	    then
+	        echo "Removing \"$VHOSTDOCROOT\"..."
+	        rm -fR $VHOSTDOCROOT
+	    else
+	        echo "NOT removing \"$VHOSTDOCROOT\"..."
+	    fi
     else
-	echo "\"$VHOSTDOCROOT\" doesn't exist, so not offering to remove it..."
+	    echo "\"$VHOSTDOCROOT\" doesn't exist, so not offering to remove it..."
     fi
 
     # Remove virtualhost log root if it exists
     if [ -d $VHOSTLOGROOT ]
     then
-	echo "Removing \"$VHOSTLOGROOT\"..."
-	rm -fR $VHOSTLOGROOT
+	    echo "Removing \"$VHOSTLOGROOT\"..."
+    	rm -fR $VHOSTLOGROOT
     else
-	echo "Thereis no \"$VHOSTLOGROOT\", nothing remove..."
+	    echo "There is no \"$VHOSTLOGROOT\", nothing remove..."
     fi
 
     # Remove line from /etc/hosts if it is there
     grep -Fxq "$HOSTSLINE" "/etc/hosts"
     if [ $? = 0 ]
     then
-	echo "Removing \"$HOSTSLINE\" from \"/etc/hosts\"..."
-	sudo sed -i "/$HOSTSLINE/d" /etc/hosts
+	    echo "Removing \"$HOSTSLINE\" from \"/etc/hosts\"..."
+	    sudo sed -i "/$HOSTSLINE/d" /etc/hosts
     else
-	echo "\"$HOSTSLINE\" not inside \"/etc/hosts\", so not removing..."
+	    echo "\"$HOSTSLINE\" not inside \"/etc/hosts\", so not removing..."
     fi
 
     # Remove virtual host file if it exists
     if [ -f $VHOSTFILE ]
     then
-	echo "Removing \"$VHOSTFILE\"..."
-	rm $VHOSTFILE
+	    echo "Removing \"$VHOSTFILE\"..."
+	    rm $VHOSTFILE
     else
-	echo "\"$VHOSTFILE\" doesn't exist, so not removing..."
+	    echo "\"$VHOSTFILE\" doesn't exist, so not removing..."
+    fi
+
+    # Remove SSL virtual host file if it exists
+    if [ -f $VHOSTFILESSL ]
+    then
+	    echo "Removing \"$VHOSTFILESSL\"..."
+	    rm $VHOSTFILESSL
+    else
+	    echo "\"$VHOSTFILESSL\" doesn't exist, so not removing..."
     fi
 
     # If MySQL credentials are available, use them to remove db and user
     if [[ ! -z $MYSQLAU ]] || [[ ! -z $MYSQLAP ]]
     then
-	yes_no_pause "Do you want to remove MySQL \"$NAME\" database and \"$NAME\" user?"
-	if [ $? = 0 ]
-	then
-	    echo "Removing MySQL \"$MYSQLU\" user and \"$MYSQLN\" database..."
-mysql "-u$MYSQLAU" "-p$MYSQLAP" <<QUERY_INPUT
+	    yes_no_pause "Do you want to remove MySQL \"$MYSQLN\" database and \"$MYSQLU\" user?"
+	    if [ $? = 0 ]
+	    then
+	        echo "Removing MySQL \"$MYSQLU\" user and \"$MYSQLN\" database..."
+MYSQL_PWD=$MYSQLAP mysql "-u$MYSQLAU" <<QUERY_INPUT
 GRANT USAGE ON * . * TO '$MYSQLU'@'localhost';
 DROP USER '$MYSQLU'@'localhost';
 DROP DATABASE IF EXISTS \`$MYSQLN\`;
 QUERY_INPUT
-	else
-	    "Not removing MySQL \"$MYSQLN\" database and \"$MYSQLU\" user..."
-	fi
+	    else
+	        "Not removing MySQL \"$MYSQLN\" database and \"$MYSQLU\" user..."
+	    fi
     else
-	echo "Ommit removing MySQL user and database..."
+	    echo "Omit removing MySQL user and database..."
     fi
 
     # Disable virtual host
@@ -320,54 +356,60 @@ QUERY_INPUT
 # We need admin privileges to proceed
 if [ "$(whoami)" != "root" ]
     then
-    exit_pause "Please call this script with elevated privileges."
+        exit_pause "Please call this script with elevated privileges."
 fi
 
 # Parse script arguments
-while getopts "hm:n:t:d:u:p:U:P:N:gc" OPTION
+while getopts "hm:n:t:d:u:p:U:P:N:o:s:gcS" OPTION
 do
   case $OPTION in
     h)
-      usage
-      exit 1
-      ;;
+        usage
+        exit 1
+        ;;
     m)
-      MODE=$OPTARG
-      ;;
+        MODE=$OPTARG
+        ;;
     n)
-      NAME=$OPTARG
-      ;;
+        NAME=$OPTARG
+        ;;
     t)
-      TLD=$OPTARG
-      ;;
+        TLD=$OPTARG
+        ;;
     d)
-      DOCROOT=$OPTARG
-      ;;
+        DOCROOT=$OPTARG
+        ;;
     u)
-      MYSQLAU=$OPTARG
-      ;;
+        MYSQLAU=$OPTARG
+        ;;
     p)
-      MYSQLAP=$OPTARG
-      ;;
+        MYSQLAP=$OPTARG
+        ;;
     U)
-      MYSQLU=$OPTARG
-      ;;
+        MYSQLU=$OPTARG
+        ;;
     P)
-      MYSQLP=$OPTARG
-      ;;
+        MYSQLP=$OPTARG
+        ;;
     N)
-      MYSQLN=$OPTARG
-      ;;
+        MYSQLN=$OPTARG
+        ;;
+    o)
+        PORT=$OPTARG
+        ;;
+    s)
+        PORTSSL=$OPTARG
+        ;;
     g)
-      GIT=true
-      ;;
-    c)
-      CGI=true
-      ;;
+        GIT=true
+        ;;
+    S)
+        SSL=true
+        ;;
     ?)
-      usage
-      exit 1
-      ;;
+        usage
+        exit 1
+        ;;
   esac
 done
 
@@ -402,12 +444,23 @@ then
      MYSQLN=$NAME
 fi
 
+# MySQL login data is limited to 16 characters
+MYSQLU=${MYSQLU:0:16}
+MYSQLP=${MYSQLP:0:16}
+MYSQLN=${MYSQLN:0:16}
+
 # If $TLD specified, use it as vhost domain
 if [[ ! -z $TLD ]]
 then
      VHOSTDOMAIN="$NAME.$TLD"
 else
      VHOSTDOMAIN="$NAME"
+fi
+
+# If $SSL specified, do SSL virtual host file
+if [[ ! -z $SSL ]]
+then
+     VHOSTFILESSL="/etc/apache2/sites-available/$NAME-ssl.conf"
 fi
 
 # Virtual host file
@@ -418,9 +471,6 @@ VHOSTDOCROOT="$DOCROOT/$NAME"
 
 # Virtual host log root
 VHOSTLOGROOT="$LOGROOT/$NAME"
-
-# Virtual host CGI root
-VHOSTCGIROOT="$DOCROOT/$NAME/cgi-bin"
 
 # Virtual host /etc/hosts line
 HOSTSLINE="127.0.0.1 $VHOSTDOMAIN"
